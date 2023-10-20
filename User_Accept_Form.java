8b18be694c7f77010fdf8bf551918a4fed0b2c27
Warisa Khan <112403851+warisakhan@users.noreply.github.com>
2023-10-20 18:16:43 -0400

    Delete User_Accept_Form.java

--- User_Accept_Form.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*; 
-import org.junit.jupiter.api.Test;
-
-public void test(){
-    User user = new User();
-    
-    assertThat(user.acceptForm(), is("Form accepted"));
-}
