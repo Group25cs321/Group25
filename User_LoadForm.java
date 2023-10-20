a676bdb216d8e97a687cb47947237296832f80a7
Warisa Khan <112403851+warisakhan@users.noreply.github.com>
2023-10-20 19:10:18 -0400

    Add files via upload

---
+++ User_LoadForm.java
@@ -0,0 +1,14 @@
+/*imports for testing */
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*; 
+import org.junit.jupiter.api.Test;
+
+@Test
+@DisplayName("The form is able to be seen by the user.")
+public void loadForm_test(){
+    /*creating a new object of class User */
+    User user = new User();
+    
+    /* return is empty because this method is not needed for the user class, however it was implemented as it is a public method so the user class still has access*/
+    assertNull(user.loadForm());
+}
