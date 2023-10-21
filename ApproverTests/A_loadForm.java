899df60a04c67d9586e7025563ac5329194893a9
noorsajidd <146343687+noorsajidd@users.noreply.github.com>
2023-10-20 20:41:27 -0400

    Update A_loadForm.java

--- ApproverTests/A_loadForm.java
+++ ApproverTests/A_loadForm.java
@@ -1,12 +1,19 @@
+/*Imports for tests*/
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class Approver{
     @Test
-    public void loadFormTest(){
-        
-    }
+    @DisplayName("Can load form")
+    public void approverLoadFormTest(){
+        /*New approver object of Approver class */
+        Approver approver = new Approver();
 
-    assert 
+        /*New form object of Form class */
+        Form form = new Form();
+
+        /*Returns string if the approver can load form */
+        assertEquals(Approver.loadForm(Form), "Form has been accessed by Approver.");
+    }
 }
