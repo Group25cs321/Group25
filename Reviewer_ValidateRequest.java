5c66e2cb2209dc0c8cf10ed1a7fbdb1a96bfa9bc
Group25cs321 <148501669+Group25cs321@users.noreply.github.com>
2023-10-20 20:55:13 -0400

    Add files via upload

---
+++ Reviewer_ValidateRequest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class Reviewer { 
+
+    @Test
+    @DisplayName("Reviewer has validated reuqest")
+    public void validateRequest(){
+        //New Reviewer object of reviewer class/
+        Reviewer reviewer = new Reviewer()
+
+
+        //Returns string if reviewer validated request      
+
+        assertEquals(Reviewer.validateRequest(), "Reviewer has successfully validated reuqest");
+    }
+}
