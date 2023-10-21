a28f566e48dba578d433b0d21bea692e1c6b84dc
noorsajidd <146343687+noorsajidd@users.noreply.github.com>
2023-10-20 20:40:02 -0400

    Update A_approveInfo.java

--- ApproverTests/A_approveInfo.java
+++ ApproverTests/A_approveInfo.java
@@ -1,1 +1,16 @@
+/*Imports for tests*/
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+ 
+public class Approver{
+    @Test
+    @DisplayName("Approver can approve form")
+    public void approverApprovesTest(){
+        /*New approver object of Approver class */
+        Approver approver = new Approver();
 
+        /*Returns string if the approver approves info */
+        assertEquals(approver.approveInfo(), "Correct information on form.");
+    }
+}
