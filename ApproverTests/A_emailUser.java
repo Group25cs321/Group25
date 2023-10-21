ca86cba79699cc128e708b58efbeef4090c6c450
noorsajidd <146343687+noorsajidd@users.noreply.github.com>
2023-10-20 20:40:49 -0400

    Update A_emailUser.java

--- ApproverTests/A_emailUser.java
+++ ApproverTests/A_emailUser.java
@@ -1,1 +1,16 @@
+/*Imports for tests*/
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
 
+public class Approver{
+    @Test
+    @DisplayName("Approver can email user")
+    public void approverEmailsTest(){
+        /*New approver object of Approver class */
+        Approver approver = new Approver();
+
+        /*Returns string if the approver can email user */
+        assertEquals(approver.emailUser(), "User has been emailed.");
+    }
+}
