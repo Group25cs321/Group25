efdcb378d1bfb830a9a85b80cbc24c461c993bbf
noorsajidd <146343687+noorsajidd@users.noreply.github.com>
2023-10-20 20:40:23 -0400

    Update A_denyInfo.java

--- ApproverTests/A_denyInfo.java
+++ ApproverTests/A_denyInfo.java
@@ -1,1 +1,16 @@
+/*Imports for tests*/
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
 
+public class Approver{
+    @Test
+    @DisplayName("Approver can deny form")
+    public void approverDeniesTest(){
+        /*New approver object of Approver class */
+        Approver approver = new Approver();
+
+        /*Returns string if the approver denies info */
+        assertEquals(approver.denyInfo(), "Incorrect information on form.");
+    }
+}
