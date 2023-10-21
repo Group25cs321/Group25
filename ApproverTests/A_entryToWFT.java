d7c9a0cccc2e3f4ec8814ffde124b8038e9ac94d
noorsajidd <146343687+noorsajidd@users.noreply.github.com>
2023-10-20 20:41:00 -0400

    Update A_entryToWFT.java

--- ApproverTests/A_entryToWFT.java
+++ ApproverTests/A_entryToWFT.java
@@ -1,1 +1,19 @@
+/*Imports for tests*/
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+ 
+public class Approver{
+    @Test
+    @DisplayName("Approver can create WTF entry")
+    public void approverWFTEntryTest(){
+        /*New approver object of Approver class */
+        Approver approver = new Approver();
 
+        /*New workflow object of Workflow class */
+        Workflow workflow = new Workflow();
+
+        /*Did workflow get new entry by approver?*/
+        assertEquals(approver.entryToWFT(), "Workflow step has been created.");
+    }
+}
